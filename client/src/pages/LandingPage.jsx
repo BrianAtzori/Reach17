36ebef61584a745b34fa632339dfe3e7d1c3f35b
BrianAtzori <brianatzori@gmail.com>
2023-05-27 12:48:54 +0200

    Landing and registration selection styling

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,8 +4,8 @@ import MyImpactLogo from "../assets/myimpact-logo.svg";
 
 export default function LandingPage() {
   return (
-    <div className="p-5">
-      <div className="min-h-screen flex items-center justify-center">
+    <div className="p-5 bg-gradient-to-t from-greensea via-jade to-emerald min-h-screen tablet:p-8">
+      <div className="flex items-center justify-center shadow-xl bg-white rounded-lg p-5 max-w-md mx-auto desktop-4k:max-w-6xl desktop-4k:rounded-2xl desktop-4k:p-12">
         <div className="text-center">
           <img
             src={MyImpactLogo}
@@ -13,19 +13,22 @@ export default function LandingPage() {
             className="mx-auto"
           />
           <p className="desktop-l:text-2xl desktop-4k:text-5xl text-gray text-lg mb-8 font-lora">
-            Benvenuto nella nostra webapp dedicata agli obiettivi di sviluppo
-            sostenibile delle Nazioni Unite.
+            Benvenut* in My Impact 🌍
+          </p>
+          <p className="desktop-l:text-2xl desktop-4k:text-5xl text-gray text-lg mb-12 font-lora">
+            La Web App dedicata agli studenti universitari e alla loro
+            formazione nell'ambito dello sviluppo sostenibile 🤝🏻
           </p>
           <div className="flex justify-center gap-3 text-gray desktop-l:gap-5 desktop-4k:gap-8">
             <Link
               to="/login"
-              className=" desktop-l:text-2xl desktop-4k:text-5xl font-montserrat bg-transparent border text-gray rounded-md py-2 px-4 hover:bg-white hover:text-jade hover:animate-bounce"
+              className=" desktop-l:text-2xl desktop-4k:text-5xl font-montserrat bg-transparent border text-gray rounded-md py-2 px-4 hover:bg-white hover:text-jade"
             >
               Accedi
             </Link>
             <Link
               to="/select-account-type"
-              className="desktop-l:text-2xl desktop-4k:text-5xl bg-white font-montserrat hover:text-white text-gray rounded-md py-2 px-4 mr-4 hover:bg-jade hover:animate-bounce"
+              className="desktop-l:text-2xl desktop-4k:text-5xl bg-white font-montserrat hover:text-white text-gray rounded-md py-2 px-4 mr-4 hover:bg-jade"
             >
               Registrati
             </Link>
--- client/src/pages/SelectRegistrationPage.jsx
+++ client/src/pages/SelectRegistrationPage.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SelectRegistrationPage() {
   const registrationData = [
     {
+      emoji:"👩🏻‍🎓👨🏽‍🎓👨🏼‍🎓🧑🏾‍🎓",
       title: "Studente",
-      description: "Registrazione per utenti di tipo A",
+      description: "Registrazione per tutti gli studenti",
       link: "/sign-up",
       color: "bg-blue-500",
     },
     {
+      emoji:"👩🏼‍🏫👨🏻‍🏫👨🏿‍🏫🧑🏽‍🏫",
       title: "Professore",
       description: "Registrazione per utenti di tipo B",
       link: "/registrazione/utenteB",
       color: "bg-green-500",
     },
     {
+      emoji:"🏫🎓✏️📚",
       title: "Ateneo",
       description: "Registrazione per utenti di tipo C",
       link: "/registrazione/utenteC",
@@ -23,17 +27,17 @@ export default function SelectRegistrationPage() {
   ];
 
   return (
-    <div>
-      <div className="flex justify-center">
-        {registrationData.map((data, index) => (
-          <a
-            key={index}
-            href={data.link}
-            className={`w-64 m-4 p-6 text-center rounded-lg text-jade ${data.color}`}
-          >
-            <h2 className="text-2xl font-bold mb-4">{data.title}</h2>
-            <p>{data.description}</p>
-          </a>
+    <div className="flex justify-center p-5 bg-gradient-to-t from-greensea via-jade to-emerald min-h-screen tablet:p-8">
+      <div className="w-full bg-gray flex justify-around flex-wrap p-8 gap-3">
+        {registrationData.map((data) => (
+          <div className="flex items-center justify-center shadow-xl bg-white rounded-lg p-5 max-w-md mx-auto desktop-4k:max-w-6xl desktop-4k:rounded-2xl desktop-4k:p-12">
+            <div className="flex flex-col justify-around items-center gap-4">
+              <p className="text-4xl mb-4">{data.emoji}</p>
+              <h2 className=" font-semibold text-jade font-lora">{data.title}</h2>
+              <p>{data.description}</p>
+              <Link to={data.link}> <button>Registrati</button></Link>
+            </div>
+          </div>
         ))}
       </div>
     </div>
