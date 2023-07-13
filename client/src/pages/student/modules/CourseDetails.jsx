1b9919a129c2ad3c20a3b091095ff34c876b2e6d
BrianAtzori <brianatzori@gmail.com>
2023-07-13 08:17:36 +0200

    Course details visualization

--- client/src/pages/student/modules/CourseDetails.jsx
+++ client/src/pages/student/modules/CourseDetails.jsx
@@ -4,15 +4,13 @@ import { useState } from "react";
 import { getCourse } from "../../../services/student/external-calls";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  getSingleItemByID,
-  getAllUsersByCategory,
-} from "../../../services/utilities/external-calls";
+import { getSingleItemByID } from "../../../services/utilities/external-calls";
 import nextId from "react-id-generator";
 import _ from "lodash";
+import { courseSignUp } from "../../../services/student/external-calls";
 
 export default function CourseDetails() {
-  const [editedCourse, setEditedCourse] = useState({
+  const [selectedCourse, setselectedCourse] = useState({
     title: "",
     description: "",
     teacher: "",
@@ -22,11 +20,17 @@ export default function CourseDetails() {
     type: "",
   });
 
-  const [universitiesList, setUniversitiesList] = useState([]);
-
   const [selectedUniversities, setSelectedUniversities] = useState([]);
 
-  const [newUniversity, setNewUniversity] = useState({});
+  const [selectedTeacher, setSelectedTeacher] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    degrees: "",
+    courses: [""],
+    universities: [""],
+  });
 
   const navigator = useNavigate();
 
@@ -38,28 +42,19 @@ export default function CourseDetails() {
 
   async function retrieveData() {
     let course = await retrieveCourseData(id);
-    setEditedCourse(course);
-
-    let universities = await retrieveAllUniversities();
-    setUniversitiesList(universities);
+    setselectedCourse(course);
 
     let selectedUnis = await retrieveSelectedUniversities();
     setSelectedUniversities(selectedUnis);
+
+    let teacher = await retrieveSingleTeacher();
+    setSelectedTeacher(teacher);
   }
 
   async function retrieveCourseData(courseID) {
     let selectedCourseData = {};
     selectedCourseData = await getCourse(courseID);
     return selectedCourseData;
-  }
-
-  async function retrieveAllUniversities() {
-    const { universities } = await getAllUsersByCategory(
-      "studentData",
-      "universities"
-    );
-
-    return universities;
   }
 
   async function retrieveSelectedUniversities() {
@@ -102,52 +97,79 @@ export default function CourseDetails() {
     return university;
   }
 
+  async function retrieveSingleTeacher() {
+    let retrievedTeacher = [];
+
+    retrievedTeacher = await retrieveCourseData(id)
+      .then((data) => {
+        return data;
+      })
+      .then(async (course) => {
+        let fetchedTeacher = [];
+        fetchedTeacher = await await getSingleItemByID(
+          "studentData",
+          "teachers",
+          course.teacher
+        );
+        return fetchedTeacher.teacher[0];
+      });
+
+    return retrievedTeacher;
+  }
+
+  async function signUpForCourse(id) {
+    await courseSignUp(id);
+    navigator("/student/dashboard/");
+  }
+
   return (
-    <div className="p-5 bg-gradient-to-t from-greensea via-jade to-emerald h-screen">
+    <div className="p-5 bg-gradient-to-t from-greensea via-jade to-emerald min-h-screen h-fit">
       <div className="w-full tablet:w-2/3 desktop:w-2/3 desktop-l:w-3/5 desktop-4k:w-3/4 desktop-4k:text-4xl shadow-xl bg-white rounded-lg p-5 max-w-md mx-auto desktop-4k:max-w-6xl desktop-4k:rounded-2xl desktop-4k:p-12">
         <h1 className="text-2xl font-bold mb-4 desktop-4k:text-4xl font-lora text-gray">
-          Dettagli corso
+          Dettagli del corso{" "}
+          <span className="underline">{selectedCourse.title}</span>
         </h1>
-        <div
-          className="pt-6 mb-4 items-start font-montserrat text-gray w-full"
-        >
-          <div className="mb-4">
-            <label
-              className="block text-sm font-bold mb-2 desktop-4k:text-4xl"
-              htmlFor="title"
-            >
-              Titolo del corso
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="title"
-              type="text"
-              placeholder="Inserisci il nuovo titolo per il tuo corso"
-              value={editedCourse.title}
-              readOnly
-            />
-          </div>
+        <div className="mb-4 items-start font-montserrat text-gray w-full">
           <div className="mb-4 ">
-            <label
-              className="block text-sm font-bold mb-2 desktop-4k:text-4xl"
-              htmlFor="description"
-            >
-              Descrizione del corso
-            </label>
-            <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="description"
-              placeholder="Modifica la descrizione del tuo corso"
-              value={editedCourse.description}
-              readOnly
-            />
+            <p>{selectedCourse.description}</p>
           </div>
-          <div className="mb-4">
+          <div className="my-6 items-start font-montserrat text-gray w-full">
+            <label className="text-xl font-semibold">
+              Il tuo insegnante sarà:
+            </label>
+            <img
+              className=" mt-4 w-32 h-32 rounded-full mx-auto"
+              src="https://picsum.photos/200"
+              alt="Profile"
+            />
+            <h2 className="text-center text-2xl font-semibold mt-3">
+              {selectedTeacher.name + " " + selectedTeacher.surname}
+            </h2>
+            <div className="flex justify-center mt-5">
+              <a href={"mailto:" + selectedTeacher.email} className="underline">
+                {selectedTeacher.email}
+              </a>
+            </div>
+            <div className="mt-5">
+              <h3 className="block text-sm font-bold mb-2 desktop-4k:text-4xl">
+                Lauree e certificazioni:
+              </h3>
+              <p className="mt-2">{selectedTeacher.degrees}</p>
+            </div>
+          </div>
+          <div className="mt-5">
+            <h3 className="block text-sm font-bold mb-2 desktop-4k:text-4xl">
+              Tipologia corso:
+            </h3>
+            Il corso è di tipo {selectedCourse.type} per un totale di{" "}
+            {selectedCourse.hours} ore di formazione.
+          </div>
+          <div className="mt-8">
             <label
               className="block text-sm font-bold mb-2 desktop-4k:text-4xl"
               htmlFor="University"
             >
-              Attualmente il tuo corso è erogato in questi Atenei:
+              Il corso è erogato anche in altri atenei:
             </label>
             <div className="flex flex-row flex-wrap gap-2 justify-around">
               {selectedUniversities.map((university) => {
@@ -162,73 +184,13 @@ export default function CourseDetails() {
               })}
             </div>
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-sm font-bold mb-2 desktop-4k:text-4xl"
-              htmlFor="University"
-            >
-              Seleziona qui un nuovo Ateneo:
-            </label>
-            <select
-              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="universities"
-              name="universities"
-              value={newUniversity}
-              placeholder="Seleziona un nuovo ateneo"
-              readOnly
-            >
-              <option value="">Seleziona un nuovo ateneo</option>
-              {universitiesList.map((university) => {
-                return (
-                  <option key={nextId()} value={university._id}>
-                    {university.universityName}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-sm font-bold mb-2 desktop-4k:text-4xl"
-              htmlFor="hours"
-            >
-              Numero ore corso
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="hours"
-              type="number"
-              placeholder="Inserisci il numero di ore del tuo corso"
-              value={editedCourse.hours}
-              readOnly
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-sm font-bold mb-2 desktop-4k:text-4xl"
-              htmlFor="type"
-            >
-              Tipologia di corso
-            </label>
-            <select
-              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="type"
-              name="type"
-              value={editedCourse.type}
-              readOnly
-            >
-              <option value="">Seleziona la tipologia di corso</option>
-              <option value="Teorico">Teorico</option>
-              <option value="Pratico">Pratico</option>
-            </select>
-          </div>
           <div className="flex items-center justify-center gap-10">
-            <input
-              className=" w-full border-2 text-white bg-greensea mt-5 font-bold rounded focus:outline-none focus:shadow-outline max-w-md desktop-4k:p-4 desktop-4k:rounded-xl desktop-4k:mt-20 hover:bg-transparent hover:text-emerald hover:border-2 hover:border-solid"
-              type="submit"
-              value="Modifica corso"
-              readOnly
-            ></input>
+            <button
+              onClick={() => signUpForCourse(selectedCourse._id)}
+              className="w-full border-2 text-white bg-greensea mt-5 font-bold rounded focus:outline-none focus:shadow-outline max-w-md desktop-4k:p-4 desktop-4k:rounded-xl desktop-4k:mt-20 hover:bg-transparent hover:text-emerald hover:border-2 hover:border-solid"
+            >
+              Iscriviti al corso
+            </button>
           </div>
         </div>
       </div>
